2b6460c7871a21faf2d8ec28ec849c3c382788c3
livednoiz <sgebel23@gmail.com>
2025-11-21 21:26:08 +0100

    Angular setup basic structure web app RA Gaussmann

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Importing components for the routes
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Services } from './pages/services/services';
+import { Imprint } from './pages/imprint/imprint';
+
+export const routes: Routes = [
+  { 
+    path: '', component: Home, 
+    title: 'Kanzlei Gaussmann – Startseite'
+  },
+  {
+    path: 'ueber', component: About,
+    title: 'Über die Kanzlei – Rechtsanwalt Gaussmann'
+  },
+  {
+    path: 'leistungen', component: Services,
+    title: 'Rechtsgebiete – Kanzlei Gaussmann'
+  },
+  {
+    path: 'kontakt', component: Contact,
+    title: 'Kontakt & Anfahrt – Kanzlei Gaussmann'
+  },
+  {
+    path: 'impressum', component: Imprint,
+    title: 'Impressum – Kanzlei Gaussmann'
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
