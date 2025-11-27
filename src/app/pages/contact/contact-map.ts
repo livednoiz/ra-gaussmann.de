78067248f0b8bf97732ff9cc8adccd8ffe9ead66
livednoiz <sgebel23@gmail.com>
2025-11-27 19:26:57 +0100

    Setup maplibre stadimaps.com api_key

--- src/app/pages/contact/contact-map.ts
+++ src/app/pages/contact/contact-map.ts
@@ -1,4 +1,5 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import maplibregl from 'maplibre-gl';
 
 @Component({
@@ -8,20 +9,29 @@ import maplibregl from 'maplibre-gl';
   styleUrls: ['./contact-map.css']
 })
 export class ContactMap implements AfterViewInit {
-  @ViewChild('map', { static: true }) mapContainer!: ElementRef<HTMLDivElement>;
+  @ViewChild('map', { static: false }) mapContainer!: ElementRef<HTMLDivElement>;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit(): void {
-    if (typeof window !== 'undefined') {
-      const map = new maplibregl.Map({
-        container: this.mapContainer.nativeElement,
-        style: 'https://tiles.stadiamaps.com/styles/osm_bright.json',
-        center: [11.843935, 48.813252], // lon, lat (Abensberg)
-        zoom: 15
-      });
+    if (!isPlatformBrowser(this.platformId)) return;
 
-      new maplibregl.Marker()
-        .setLngLat([11.843935, 48.813252])
-        .addTo(map);
-    }
+    setTimeout(() => {
+      if (!this.mapContainer?.nativeElement) return;
+      this.initMap();
+    });
+  }
+
+  private initMap() {
+    const map = new maplibregl.Map({
+      container: this.mapContainer.nativeElement,
+      style: 'https://tiles.stadiamaps.com/styles/osm_bright.json?api_key="f4a2b3f6-c7bd-470b-a296-96cd99ab1249"',
+      center: [11.843935, 48.813252],
+      zoom: 15
+    });
+
+    new maplibregl.Marker()
+      .setLngLat([11.843935, 48.813252])
+      .addTo(map);
   }
 }
